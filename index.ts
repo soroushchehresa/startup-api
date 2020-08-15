511c14580042b749ffb131cadcab2d289da5cd16
Soroush Chehresa <s1996ch@gmail.com>
2020-08-16 00:48:53 +0430

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
 export default async (req: Request, res: Response) => {
-  res.json({version: '1.0.0'});
+  res.json({
+    androidCurrentVersion: '1.0.0',
+    androidMinVersion: '1.0.0',
+    iosCurrentVersion: '1.0.0',
+    iosMinVersion: '1.0.0',
+    playStore: 'https://',
+    appStore: 'https://',
+  });
 };
